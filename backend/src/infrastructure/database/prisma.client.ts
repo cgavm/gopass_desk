f0c37909d8906cb03fd12a978f84f9a2763e643b
cgav <cristian.gaviria@bpsmart.ai>
2026-06-12 13:02:40 -0500

    fix: removed unnecesary logger

--- backend/src/infrastructure/database/prisma.client.ts
+++ backend/src/infrastructure/database/prisma.client.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { logger } from '../cache/redis.client';
+
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
